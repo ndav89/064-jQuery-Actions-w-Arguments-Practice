8a88d7726d573877c49150d5acb5170679e9a98f
ndav89 <105799691+ndav89@users.noreply.github.com>
2022-05-18 09:11:44 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+// Guided Practice
+$(".reply-yes").click(function() {  
+    $(".homers-reply").text("Woo Hoo!");
+    $(".reply-yes").css("background-color", "yellow");
+});
+
+//Independent Practice (START YOUR CODE BELOW!)
+$(".reply-no").click(function() {  
+      $(".homers-reply").text("D'oh");
+      $("h1").css("color", "yellow");
+});
+
